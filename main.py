381afe77d92542421309924109736fc2f77d9bf9
ianjali <amudgal26@gwmail.gwu.edu>
2025-01-21 19:18:26 +0530

    data ingestion stage 1 pipeline

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
 from src.TextSummarizer.logging import logger
-logger.info("Logging is implemented successfully")
+from src.TextSummarizer.pipeline.stage_1_data_ingestion_pipeline import DataIngestionTrainingPipeline
+
+# logger.info("Logging is implemented successfully")
+STAGE_NAME="Data Ingestion stage"
+try:
+    logger.info(f"stage {STAGE_NAME} initiated")
+    data_ingestion_pipeline=DataIngestionTrainingPipeline()
+    data_ingestion_pipeline.initiate_data_ingestion()
+    logger.info(f"stage {STAGE_NAME} completed")
+except Exception as e:
+    logger.exception(f"stage {STAGE_NAME} failed : {str(e)}")
+    raise e
+      
